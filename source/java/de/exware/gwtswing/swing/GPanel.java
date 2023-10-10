13f1562f1b274e3eb5865fe664e3eeaf5a500c74
pizzadox9999 <hanzmeier70@web.de>
2023-10-10 19:05:28 +0200

    added constructor with GLayoutManager

--- source/java/de/exware/gwtswing/swing/GPanel.java
+++ source/java/de/exware/gwtswing/swing/GPanel.java
@@ -1,8 +1,14 @@
 package de.exware.gwtswing.swing;
 
+import de.exware.gwtswing.awt.GLayoutManager;
+
 public class GPanel extends GComponent
 {
     public GPanel()
     {
+    }
+
+    public GPanel(GLayoutManager layout) {
+        setLayout(layout);
     }
 }
