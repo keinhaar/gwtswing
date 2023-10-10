3cb1f5058c4323d3977439b5b096dd475b47129e
pizzadox9999 <hanzmeier70@web.de>
2023-10-10 19:05:50 +0200

    added relevant constants

--- source/java/de/exware/gwtswing/swing/GSwingConstants.java
+++ source/java/de/exware/gwtswing/swing/GSwingConstants.java
@@ -2,7 +2,12 @@ package de.exware.gwtswing.swing;
 
 public interface GSwingConstants
 {
-    public static final int CENTER = 0;
-    public static final int HORIZONTAL = 0;
-    public static final int VERTICAL = 1;
+    public static final int CENTER      = 0;
+    public static final int TOP         = 1;
+    public static final int LEFT        = 2;
+    public static final int BOTTOM      = 3;
+    public static final int RIGHT       = 4;
+    
+    public static final int HORIZONTAL  = 0;
+    public static final int VERTICAL    = 1;
 }
